7248c297102131d65d006a628b170039632bb120
= <nicolas.arcas1@gmail.com>
2021-07-19 15:33:51 -0300

    Inicializando o projeto

---
+++ src/main/java/com/alurachallengebackend/AluraChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alurachallengebackend/AluraChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
